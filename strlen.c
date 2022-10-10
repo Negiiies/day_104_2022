ca71ebea4324f7bcfd248488179e1c09077ff794
LE Dylan <dylan.le@ecole-89.com>
2022-10-10 11:04:34 +0200

    strlen

---
+++ strlen.c
@@ -0,0 +1,9 @@
+unsigned int stu_strlen(const char *str)
+{
+    int i = 0;
+    while (str[i] != ('\0'))
+        i +=1;
+    {
+        return (i);
+    }
+}
---
+++ test_strlen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+unsigned int stu_strlen(const char *str);
+int main(void)
+{
+    return (stu_strlen("Boire de l'eau c'est mal "));
+}
