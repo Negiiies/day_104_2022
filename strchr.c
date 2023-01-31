6dbee06e07dc132222fe8cf1e94c25087ba3a320
LE Dylan <dylan.le@ecole-89.com>
2023-01-31 11:13:36 +0100

    update

--- strchr.c
+++ strchr.c
@@ -2,18 +2,14 @@
 char *stu_strchr(const char *str, char search)
 {
     int i;
+
     i = 0;
-    int y;
-    y = 0;
-    while (str[i] != '\0')
-        {
-            i +=1;
-        }
-    if(search[y] != str[i]){
-        search[y] += 1;
-        search[y] = str[i];
-        return (char*)str;
-    } else {
-        return (NULL);
+    if (!str) {
+        return NULL;
     }
+    if (search = str[i]) {
+        return (char*)str;
+    }
+    str += 1;
+    return 0;
 }
